feb21297f226da046f0ac13cad9afa45baeb7bb2
turkivv66 <turkovv66k@gmail.com>
2026-01-21 17:22:31 +0300

    WIP

--- snakecutting.cpp
+++ snakecutting.cpp
@@ -1,5 +1,5 @@
 #include "snakecutting.h"
-
+#include <QDebug>
 
 snakeCutting::snakeCutting()
 {}
@@ -8,7 +8,8 @@ snakeCutting::snakeCutting()
 // X ось x, Y ось y, Z ось z
 void snakeCutting::moveTo(double X, double Y, double Z)
 {
-
+    qDebug() << "X:" << X << "Y:" << Y << "Z:" << Z;
+    qDebug() << "----------------------";
 }
 
 void snakeCutting::cutting(Mill mill, Key key)
@@ -26,36 +27,46 @@ void snakeCutting::cutting(Mill mill, Key key)
     // 1 проход если ширина змейки от 1 до 2 диаметров фрезы
     if ((mill.D <= Hzm) && (Hzm <= 2 * mill.D))
     {
-        for (int i = Zdept / mill.DeltaH; i > 0; i--)
+        for (int i = Zdept / mill.DeltaH; i >= 0; i--)  // TO DO сделать проверку если глубина не делится на кол во
+                                                        // проходов
         {
             // опускаемся по z на высоту ключа - iый проход
             snakeCutting::moveTo(cordS.X0 + cuts.last().L + mill.D / 2,
                                  cordS.Y0 + key.L + 2 * mill.D,
-                                 cordS.Z0 + key.H - mill.DeltaH);   // минус 0.2 мм
+                                 cordS.Z0 + key.H - mill.DeltaH * ((Zdept / mill.DeltaH) - i)); // минус 0.2 мм
 
-            for (int j = cuts.length(); j > 0; j--)
+            // Z не меняется во всем цикле ток Х и У
+            for (int j = cuts.length() - 1;
+                                             j >= 0;
+                                             j--)
             {
-                if (j > 1)
+                if (j > 0)
                 {
                     // по Х тоже самое
                     snakeCutting::moveTo(cordS.X0 + cuts[j].L + mill.D / 2,
-                                     cordS.Y0 + (key.L - cuts[j].B) - cuts[j].D / 2 - deltaD,
-                                     cordS.Z0 + key.H - mill.DeltaH);
+                                     cordS.Y0 + key.L - ((key.L - cuts[j].B) - cuts[j].D / 2 - deltaD),
+                                     cordS.Z0 + key.H - mill.DeltaH * ((Zdept / mill.DeltaH) - i));
                 }
 
                 // если вырез последний режем доп площадку равную D фрезы запасом
-                if (j == 1)
+                if (j == 0)
                 {
                     snakeCutting::moveTo(cordS.X0 + cuts[j].L + mill.D / 2,
-                                         cordS.Y0 + (key.L - cuts[j].B) - cuts[j].D / 2 - mill.D,
-                                         cordS.Z0 + key.H - mill.DeltaH);
+                                         cordS.Y0 + key.L - ((key.L - cuts[j].B) - cuts[j].D / 2 - mill.D),
+                                         cordS.Z0 + key.H - mill.DeltaH * ((Zdept / mill.DeltaH) - i));
                 }
             }
+
+            // поднимаемся на безапосную высоту двигаемся только по Z вверх
+            // snakeCutting::moveTo( , , );
         }
     }
     else
     {   // несколько проходов если толщина больше двух диаметров фрезы и меньше ширины ключа - 2 мм (2 мм условно)
         if ((Hzm >= 2 * mill.D) && (Hzm < key.W - 2))
-        {}
+        {
+            for ()
+            {}
+        }
     }
 }
