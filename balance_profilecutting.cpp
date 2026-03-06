8b24a4fc34a233bf7fcb31e5a48d84c891230b05
turkivv66 <turkovv66k@gmail.com>
2026-03-06 15:34:49 +0300

    WIP Балансный

--- balance_profilecutting.cpp
+++ balance_profilecutting.cpp
@@ -1,3 +1,17 @@
 #include "balance_profilecutting.h"
+#include <QDebug>
 
-balance_profilecutting::balance_profilecutting() {}
+
+balance_profilecutting::balance_profilecutting()
+    : key    (new Key())
+      , mill (new Mill())
+{
+    qDebug() << "Конструктор snakeCutting вызван";
+}
+
+void balance_profilecutting::balanceCutting(Key& key, Mill& mill, QVector<balanceCut>& cuts, coordSystem& cs)
+{
+    // начало ключа
+
+    moveTo(1, 2);
+}
--- balance_profilecutting.h
+++ balance_profilecutting.h
@@ -1,9 +1,35 @@
 #pragma once
 
+#include "coordSystem.h"
+#include "Key.h"
+#include "mill.h"
+#include "QVector"
+#include <qscopedpointer.h>
+
+class PathViewer;
 
 
 class balance_profilecutting
 {
 public:
     balance_profilecutting();
+
+
+    struct balanceCut
+    {
+        double  angle1 = 0;
+        double  angle2 = 0;
+
+        // Расстояние от упорного торца до центра выреза
+        double  B = 0;
+    };
+
+    void balanceCutting(Key&, Mill&, QVector<balanceCut>&, coordSystem&);
+
+private:
+
+    void moveTo(double X, double Y);
+
+    QScopedPointer<Key>   key;
+    QScopedPointer<Mill>  mill;
 };
