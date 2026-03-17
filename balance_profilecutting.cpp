08e9a4aaf3cfbc12309ce191f373f9619e754305
beskov_notebooke <beskov.ds@niistt.ru>
2026-03-17 17:11:51 +0300

    WIP

--- balance_profilecutting.cpp
+++ balance_profilecutting.cpp
@@ -4,7 +4,7 @@
 
 balance_profilecutting::balance_profilecutting()
     : key    (new Key())
-      , mill (new Mill())
+    , mill (new Mill())
 {
     qDebug() << "Конструктор snakeCutting вызван";
 }
@@ -14,15 +14,36 @@ void balance_profilecutting::moveTo(double X, double Y, int Angle)
     qDebug() << "moveTo:" << "по X"<< X << "по Y"<< Y <<"угол поворота"<< Angle;
 };
 
-void balance_profilecutting::balanceCutting(Key& key, Mill& mill, QVector<balanceCut>& cuts, coordSystem& cs)
+void balance_profilecutting::balanceCutting(Key& key, Mill& mill, QVector<balanceCut>& cuts, coordSystem& CS)
 {
+    //заполняем массив данными
+    void fillingCuttings();
+
+    //условное безопасное расстояние до заготовки
+    double Xbez = 30;
 
     // подезжаем к первому вырезу
+    moveTo(CS.X0 + Xbez, CS.Y0, 0);
 
-    moveTo(1,2,3);
+    //проезжаем до 1 действительного выреза
+    moveTo(CS.X0 + Xbez, CS.Y0 + , 0);
+
 }
 
 
 
-
+void balance_profilecutting::fillingCuttings()
+{
+    cuts.append({0, 0});//1
+    cuts.append({1.6, 0});//2
+    cuts.append({3.2, 0});//3
+    cuts.append({5, 0});//4
+    cuts.append({5, 0});//5
+    cuts.append({5, 0});//6
+    cuts.append({5, 0});//7
+    cuts.append({5, 0});//8
+    cuts.append({5, 0});//9
+    cuts.append({5, 0});//10
+    cuts.append({5, 0});//11
+}
 
--- balance_profilecutting.h
+++ balance_profilecutting.h
@@ -17,11 +17,15 @@ public:
 
     struct balanceCut
     {
-        int  angle = 0;
-
-        // Расстояние от упорного торца до центра выреза
+        // Расстояние от упорного торца до начала выреза
         double  B = 0;
+        //угол выреза
+        int  angle = 0;
+        //ширина выреза
+        double D = 1.6;
     };
+
+
 
     void fillingCuttings();
     void balanceCutting(Key&, Mill&, QVector<balanceCut>&, coordSystem&);
@@ -30,7 +34,8 @@ private:
 
     void moveTo(double X, double Y, int Angle);
 
+    QVector<balanceCut>  cuts;
     QScopedPointer<Key>   key;
     QScopedPointer<Mill>  mill;
-    QVector<balanceCut>   cuts;
+
 };
